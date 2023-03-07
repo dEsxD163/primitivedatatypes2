23e83cceebc14e03181d2b7e84ceacb0d885b1c9
Denis <dzavalskiy@gmail.com>
2023-03-07 16:02:31 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 100; // HA C4ETY
+        int y = 1200; // CYMMA popolneniya
+        int bonus = y / 100;
+        if (y > 1000) {
+            System.out.println("Итоговая сумма:" + (x + y + bonus));
+            ;
+        } else {
+            System.out.println("Итоговая сумма:" + (x + y));
+        }
+    }
+}

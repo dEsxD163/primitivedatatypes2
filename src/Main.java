dc63bf6235a620c0d4b9ae08ac25b44a07348438
Denis <dzavalskiy@gmail.com>
2023-03-09 14:37:06 +0400

    Two commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        int money_account = 100; // Денег на счете
-        int replenishment_amount = 1200; // Сумма пополнения
-        int bonus = replenishment_amount / 100;
-        if (replenishment_amount > 1000) {
-            System.out.println("Итоговая сумма:" + (money_account + replenishment_amount + bonus));
+        int moneyAccount = 100; // Денег на счете
+        int replenishmentAmount = 1200; // Сумма пополнения
+        int bonus = replenishmentAmount / 100;
+        if (replenishmentAmount > 1000) {
+            System.out.println("Итоговая сумма:" + (moneyAccount + replenishmentAmount + bonus));
             ;
         } else {
-            System.out.println("Итоговая сумма:" + (money_account + replenishment_amount));
+            System.out.println("Итоговая сумма:" + (moneyAccount + replenishmentAmount));
         }
     }
 }
